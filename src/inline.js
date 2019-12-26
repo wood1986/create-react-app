8aab566d550ffb0890f9d44312bfe6db43f99d3d
wood </>
2019-12-25 18:52:39 -0800

    WIP

--- src/inline.js
+++ src/inline.js
@@ -1,3 +1,15 @@
-import WebWorker from "worker-loader!./web-worker";
+import Entry from "webpack-loader!./webworker.webpack.config.js";
 
-new WebWorker().addEventListener("message", console.log);
+new Worker(Entry.webworker).addEventListener("message", console.log);
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register(Entry.serviceworker).then((registration) => {
+      // Registration was successful
+      console.log("ServiceWorker registration successful with scope: ", registration.scope);
+    }, (err) => {
+      // registration failed :(
+      console.log("ServiceWorker registration failed: ", err);
+    });
+  });
+}
